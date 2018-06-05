689960e308c617c80c4c9bfa0fd08cb2917d9608
Nick <unick.zhow@gmail.com>
2018-06-05 12:34:27 +0800

    Nav and index

---
+++ src/components/AppNav.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  nav#app_nav
+    ul
+      li(v-for='tab in tabList')
+        a(
+          :class='{active: currentTab === tab}', 
+          @click='currentTab = tab') {{ tab }}
+    
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabList: [
+        "My Tasks", "In Progress", "Completed"
+      ],
+      currentTab: 'My Tasks'
+    }
+  }
+}
+</script>
